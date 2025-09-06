07da142f8c0bb23237215524274ab99eb2f49d1f
Jermaine <jermainenee@yeah.net>
2025-09-06 19:56:51 +0800

    feat: 添加基本的 Spring Boot项目结构和功能
    - 创建了项目目录结构和基本文件
    - 添加了 Spring Boot 依赖和配置
    - 实现了简单的 RESTful API 和页面渲染功能
    -包含了单元测试和文档

---
+++ src/main/java/cn/zm/ms/Microservices2025Application.java
@@ -0,0 +1,13 @@
+package cn.zm.ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Microservices2025Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Microservices2025Application.class, args);
+    }
+
+}
---
+++ src/test/java/cn/zm/ms/Microservices2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.zm.ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Microservices2025ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
